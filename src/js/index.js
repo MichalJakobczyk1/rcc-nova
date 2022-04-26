01172947397a60d812be4d0587af07a5749d6c11
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-04-26 20:35:07 +0200

    header reworked & animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,7 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  document.body.classList.remove("body--preload");
+});

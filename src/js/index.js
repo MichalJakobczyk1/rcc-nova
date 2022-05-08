d22eb7b765d30f078cb1888d6499f1770af6c334
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-05-08 11:15:31 +0200

    header transistions & buttons

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,24 @@ window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
   document.body.classList.remove("body--preload");
 });
+
+const buttonLangPl = document.querySelector(".header__button--Pl");
+const buttonLangEng = document.querySelector(".header__button--Eng");
+
+buttonLangPl.addEventListener("click", () => {
+  if (buttonLangPl.classList.contains("header__button--unactive")) {
+    buttonLangPl.classList.remove("header__button--unactive");
+    buttonLangPl.classList.add("header__button--active");
+    buttonLangEng.classList.add("header__button--unactive");
+    buttonLangEng.classList.remove("header__button--active");
+  }
+});
+
+buttonLangEng.addEventListener("click", () => {
+  if (buttonLangEng.classList.contains("header__button--unactive")) {
+    buttonLangEng.classList.remove("header__button--unactive");
+    buttonLangEng.classList.add("header__button--active");
+    buttonLangPl.classList.remove("header__button--active");
+    buttonLangPl.classList.add("header__button--unactive");
+  }
+});
